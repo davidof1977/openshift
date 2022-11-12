8bf5a5884b826c0b34a47b3f726787e163b057e5
davidof1977 <64310562+davidof1977@users.noreply.github.com>
2022-11-12 08:35:59 +0100

    Update Aplicacion.java

--- src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
+++ src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
@@ -25,7 +25,7 @@ public class Aplicacion extends HttpServlet {
 	@Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Salida GET");
-        response.getWriter().println("Salida GET version master David");  
+        response.getWriter().println("Salida GET version master David versión 2");  
         response.getWriter().flush();
     }
 
